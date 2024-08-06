3ac25ee1241bab31dc04355d4d23de29e3dd81a7
MinaDoncheva <133380007+MinaDoncheva@users.noreply.github.com>
2024-08-06 20:13:28 +0300

    move data to separate module (#3)

---
+++ codbex-number-generator/service/generator.js
@@ -0,0 +1,24 @@
+import { NumberRepository } from "../gen/dao/Numbers/NumberRepository";
+export class NumberGeneratorService {
+    constructor() {
+        this.repository = new NumberRepository();
+    }
+    generate(id) {
+        try {
+            const entity = this.repository.findById(id);
+            if (entity) {
+                entity.Value += 1;
+                this.repository.update(entity);
+                const zeroPad = (num, places) => String(num).padStart(places, '0');
+                const result = (entity.Prefix ? entity.Prefix : "") + zeroPad(entity.Value, entity.Length - (entity.Prefix ? entity.Prefix.length : 0));
+                return result;
+            }
+            else {
+                throw new Error("Number not found");
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+}
