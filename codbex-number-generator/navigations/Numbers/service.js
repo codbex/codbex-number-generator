c17482e3af6830b4ab59a490a6b10d8713826b44
Emil Momchev <80454439+Mrgoblings@users.noreply.github.com>
2025-01-27 14:34:56 +0000

    fix: file path and order

--- codbex-number-generator/navigations/Numbers/service.js
+++ codbex-number-generator/navigations/Numbers/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'numbers-navigation',
     label: "Numbers",
     group: "configurations",
-    order: 800,
+    order: 900,
     link: "/services/web/codbex-number-generator/gen/ui/Numbers/Number/index.html?embedded"
 };
 
