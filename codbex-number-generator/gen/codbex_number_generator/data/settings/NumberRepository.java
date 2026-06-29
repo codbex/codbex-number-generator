879f19c34f6c7d77f659ffe1c23b689573d4fb71
NicoleNG18 <nicole.georgieva18@abv.bg>
2026-06-29 16:00:46 +0300

    regenerate module with the latest template

--- codbex-number-generator/gen/codbex_number_generator/data/settings/NumberRepository.java
+++ codbex-number-generator/gen/codbex_number_generator/data/settings/NumberRepository.java
@@ -1,7 +1,7 @@
 package gen.codbex_number_generator.data.settings;
 
 import org.eclipse.dirigible.components.data.store.java.repository.JavaRepository;
-import org.eclipse.dirigible.engine.java.annotations.Repository;
+import org.eclipse.dirigible.sdk.component.Repository;
 
 @Repository
 public class NumberRepository extends JavaRepository<NumberEntity> {
